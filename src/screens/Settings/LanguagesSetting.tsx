b637b3054da55f35a8fbf35ac2bc6ff566a7d276
zwz <akazwz@pm.me>
2021-12-28 20:44:19 +0800

    add languages setting action sheet

--- src/screens/Settings/LanguagesSetting.tsx
+++ src/screens/Settings/LanguagesSetting.tsx
@@ -1,17 +1,33 @@
 import { useLayoutEffect } from 'react'
 import {
-  Center,
+  Actionsheet,
+  Box,
+  Flex,
+  HStack,
+  Icon,
+  Pressable,
+  ScrollView,
   StatusBar,
   Text,
-  useColorMode,
-  useColorModeValue
+  VStack,
+  Spacer,
+  useColorModeValue,
+  useDisclose,
 } from 'native-base'
+import { Ionicons } from '@expo/vector-icons'
+
+export type Language = {
+  name: string,
+  transName: string,
+  code: string,
+}
 
 const LanguagesSetting = ({ navigation }) => {
   // set navigation props
   const bgMain = useColorModeValue('#f5f5f4', '#000000')
   const bgSecond = useColorModeValue('#ffffff', '#18181b')
   const color = useColorModeValue('#27272a', '#ffffff')
+  const colorFontSecond = useColorModeValue('#5c5c60', '#adaaaa')
   useLayoutEffect(() => {
     navigation.setOptions({
       headerStyle: {
@@ -23,23 +39,97 @@ const LanguagesSetting = ({ navigation }) => {
     })
   }, [navigation, bgMain, color])
 
-  const { toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclose()
+
+  const LanguagesOptions = () => {
+    const languages: Language[] = [
+      {
+        name: '中文',
+        transName: 'English',
+        code: 'ZH',
+      },
+      {
+        name: 'English',
+        transName: 'English',
+        code: 'EN',
+      },
+    ]
+
+    const list = languages.map((item) => {
+      return (
+        <Actionsheet.Item
+          key={item.code}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <HStack>
+            <VStack w="50%">
+              <Text>
+                {item.name}
+              </Text>
+              <Text sub>
+                {item.transName}
+              </Text>
+            </VStack>
+
+            <VStack w="50%" alignItems="flex-end" justifyContent="center">
+              {item.code === 'ZH'
+                ? <Icon
+                  as={Ionicons}
+                  name="ios-checkmark"
+                  color="green.500"
+                  size="md"
+                />
+                : null
+              }
+
+            </VStack>
+          </HStack>
+        </Actionsheet.Item>
+      )
+    })
+
+    return (
+      <>{list}</>
+    )
+  }
 
   return (
-    <Center flex={1} bg={bgMain}>
-      <StatusBar
-        barStyle={useColorModeValue('dark-content', 'light-content')}
-        backgroundColor={bgMain}
-      />
-      <Text
-        bold
-        fontSize="lg"
-        display="flex"
-        mb={20}
-      >
-        Languages Setting Screen
-      </Text>
-    </Center>
+    <>
+      <Box flex={1} bg={bgMain}>
+        <StatusBar
+          barStyle={useColorModeValue('dark-content', 'light-content')}
+          backgroundColor={bgMain}
+        />
+        <ScrollView mt={1}>
+          <Pressable w="100%" alignItems="center" onPress={onOpen}>
+            <Box w="90%" bg={bgSecond} rounded="lg">
+              <Flex flexDirection="row" alignItems="center" textAlign="left" p={3}>
+                <Text>APP Language</Text>
+                <Spacer />
+                <Text color={colorFontSecond}>Chinese</Text>
+                <Icon
+                  as={Ionicons}
+                  name="ios-chevron-forward"
+                  color={colorFontSecond}
+                  size="xs"
+                />
+              </Flex>
+            </Box>
+          </Pressable>
+          <Actionsheet isOpen={isOpen} onClose={onClose}>
+            <Actionsheet.Content>
+              <Box w="100%" justifyContent="center" alignItems="center">
+                <Text>
+                  APP Languages
+                </Text>
+              </Box>
+              <LanguagesOptions />
+            </Actionsheet.Content>
+          </Actionsheet>
+        </ScrollView>
+      </Box>
+    </>
   )
 }
 
