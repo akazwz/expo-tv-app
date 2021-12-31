a72c141e6f66d39cd3a80dc719b7029f180b8d5f
akazwz <akazwz@pm.me>
2021-12-31 14:53:44 +0800

    full screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,59 +1,78 @@
-import { useLayoutEffect, useRef } from 'react'
-import {
-  AspectRatio,
-  Box,
-  ScrollView,
-  StatusBar,
-  useColorModeValue
-} from 'native-base'
-import i18n from 'i18n-js'
+import * as ScreenOrientation from 'expo-screen-orientation'
+import { Dimensions, ScrollView, StyleSheet, Text } from 'react-native'
 import { Video } from 'expo-av'
+import { setStatusBarHidden } from 'expo-status-bar'
+import React, { useRef, useState } from 'react'
+import VideoPlayer from 'expo-video-player'
 
-const HomeScreen = ({ navigation }) => {
-  // set navigation props
-  const bgMain = useColorModeValue('#f5f5f4', '#000000')
-  const bgSecond = useColorModeValue('#ffffff', '#18181b')
-  const color = useColorModeValue('#27272a', '#ffffff')
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerStyle: {
-        backgroundColor: bgMain,
-      },
-      headerTitleStyle: {
-        color: color,
-      },
-    })
-  }, [navigation, bgMain, color])
-
-  const videoRef = useRef(null)
-
+const HomeScreen = () => {
+  const [inFullscreen, setInFullsreen] = useState(false)
+  const [inFullscreen2, setInFullsreen2] = useState(false)
+  const refVideo = useRef(null)
+  const refVideo2 = useRef(null)
+  const refScrollView = useRef(null)
   return (
-    <Box
-      flex={1}
-      bg={bgMain}
-      safeArea
-      safeAreaTop={0}
+    <ScrollView
+      scrollEnabled={!inFullscreen2}
+      ref={refScrollView}
+      onContentSizeChange={() => {
+        if (inFullscreen2) {
+          refScrollView.current.scrollToEnd({ animated: true })
+        }
+      }}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
     >
-      <StatusBar
-        barStyle={useColorModeValue('dark-content', 'light-content')}
-        backgroundColor={bgMain}
+      <Text style={styles.text}>Fullscren</Text>
+      <VideoPlayer
+        videoProps={{
+          shouldPlay: false,
+          resizeMode: Video.RESIZE_MODE_CONTAIN,
+          source: {
+            uri: 'http://cctvalih5ca.v.myalicdn.com/live/cctv10_2/index.m3u8'
+          },
+          ref: refVideo2,
+        }}
+        fullscreen={{
+          inFullscreen: inFullscreen2,
+          enterFullscreen: async() => {
+            setStatusBarHidden(true, 'fade')
+            setInFullsreen2(!inFullscreen2)
+            await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT)
+            refVideo2.current.setStatusAsync({
+              shouldPlay: true,
+            })
+          },
+          exitFullscreen: async() => {
+            setStatusBarHidden(false, 'fade')
+            setInFullsreen2(!inFullscreen2)
+            await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT)
+          },
+        }}
+        style={{
+          videoBackgroundColor: 'black',
+          height: inFullscreen2 ? Dimensions.get('window').width : 160,
+          width: inFullscreen2 ? Dimensions.get('window').height : 320,
+        }}
       />
-      <ScrollView>
-        <AspectRatio w="100%" ratio={16 / 9}>
-          <Video
-            source={{ uri: 'http://cctvalih5ca.v.myalicdn.com/live/cctv10_2/index.m3u8' }}
-            rate={1.0}
-            volume={1.0}
-            isMuted={false}
-            resizeMode="cover"
-            shouldPlay
-            isLooping
-            useNativeControls
-          />
-        </AspectRatio>
-      </ScrollView>
-    </Box>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFF',
+    flex: 1,
+  },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  text: {
+    marginTop: 36,
+    marginBottom: 12,
+  },
+})
 
 export default HomeScreen
